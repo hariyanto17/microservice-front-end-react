af0c19a402a38678ff7c0a4133bfc1c0d80de5ad
hariyanto17 <hariyanto17@mhs.akba.ac.id>
2020-08-07 16:39:02 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'assets/css/style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto my-5">
+      <h1 className="text-6x1">Java script react</h1>
     </div>
   );
 }
